aedb549f7b03a9261f08cd76feb775864e15998a
grobleen <111244776+grobleen@users.noreply.github.com>
2023-07-01 14:37:01 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var resultadoprebio = "";
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelocargado)
+}
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video,gotResult);
+}
+function modelocargado(){
+console.log("modelocargado")
+}
+function gotResult(error,results){
+if(error){
+console.log(error);
+}else{
+console.log(results)
+if((results[0].confidence>0.5)&&(resultadoprebio!=results[0].label)){
+resultadoprebio=results[0].label;
+var synth=window.speechSynthesis;
+speak_data = "elobjetodetectadoes"+results[0].label;
+var utterThis=new SpeechSynthesisUtterance(speak_data);
+synth.speak(utterThis);
+}
+}
+}
